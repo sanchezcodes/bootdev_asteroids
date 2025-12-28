80d9d48acf11cf9410d3f7b3040e091528fbae1e
Luis Sanchez <lasg125@gmail.com>
2025-12-28 23:18:29 +0100

    add the clock to limit the framerate to 60fps

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 def main():
-    print("Starting Asteroids with pygame version: VERSION")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         log_state()
@@ -17,6 +16,8 @@ def main():
 
         screen.fill((0,0,0))
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()

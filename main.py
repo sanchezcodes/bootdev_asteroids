7574b5130adc6a127909f36fbb067957a8606465
Luis Sanchez <luis@Luis-FCM.local>
2025-12-28 22:24:21 +0100

    inititialize pygame asteroids project

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import constants
+
+def main():
+    print("Starting Asteroids with pygame version: VERSION")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()

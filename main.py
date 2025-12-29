22e9ecdd5184818670515400420434da0ece3985
Luis Sanchez <lasg125@gmail.com>
2025-12-29 11:46:30 +0100

    adding pygame groups for easy item management on draw and update

--- main.py
+++ main.py
@@ -7,19 +7,27 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
     
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
 
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+
+        updatable.update(dt)
+        for items in drawable:
+            items.draw(screen)
+            
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
